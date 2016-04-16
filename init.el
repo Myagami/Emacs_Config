9d55695e0e261f6c92209310c007f9439b400669
Hayate Yagami <helloproject.odoru10th@gmail.com>
2016-04-17 06:07:24 +0900

    changed Cask

---
+++ init.el
@@ -0,0 +1,50 @@
+;;load-path
+;;;emacs
+(setq load-path (cons "~/.emacs.d/" load-path))
+(setq load-path (cons "~/.emacs.d/elisp/" load-path))
+(setq load-path (cons "~/.emacs.d/elpa/" load-path))
+
+;;cask
+(require 'cask "~/.cask/cask.el")
+(cask-initialize)
+
+;;init loader
+(require 'init-loader)
+(setq init-loader-show-log-after-init 'nil)
+(init-loader-load "~/.emacs.d/inits/")
+
+;;text-encode
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default (
+	    (
+	     t (
+		:inherit nil
+		:stipple nil
+		:background "black"
+		:foreground "white"
+		:inverse-video nil
+		:box nil
+		:strike-through nil
+		:overline nil
+		:underline nil
+		:slant normal
+		:weight normal
+		:height 98
+		:width normal
+		:foundry "apple"
+		:family "Osaka")
+	       )
+	    )
+    )
+ )
+
